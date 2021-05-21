92394c60ea8fb5dfb571ea746182a6ee78e93dda
Tim Carr <carr.tim93@gmail.com>
2021-05-21 13:42:51 +0100

    housekeeping

--- rough_files/test.py
+++ rough_files/test.py
@@ -9,7 +9,7 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 
-df = pd.read_csv('creditcard.csv')
+df = pd.read_csv('../creditcard.csv')
 
 fig = go.Figure()
 
